bb59c34fbe3b1ddafd8cc3e10c92b4e6d32e2eca
luoyu-xingu <2259452261@qq.com>
2025-04-24 15:20:45 +0800

    Update ST.cpp

--- model/ST.cpp
+++ model/ST.cpp
@@ -5,10 +5,10 @@ void solve();
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-	int t = 1;
-	//std::cin>>t;
-	while(t--) solve();
-	return 0;
+    int t = 1;
+    //std::cin>>t;
+    while(t--) solve();
+    return 0;
 }
 void solve() {
 	int n, m;
